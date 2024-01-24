7bfcc297093d8042b7b7837d04d3b5f9c8a90be5
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2024-01-24 22:54:25 +0500

    first commit :)

---
+++ src/main/java/uz/laza/ecommerce/LazaEcommerceServerApplication.java
@@ -0,0 +1,13 @@
+package uz.laza.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazaEcommerceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazaEcommerceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/laza/ecommerce/LazaEcommerceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.laza.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazaEcommerceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
